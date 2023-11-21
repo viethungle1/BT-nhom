9f6de22145c87785dd4bd47bf8d4bd0ff0293710
viethungle1 <hunglv0801@gmail.com>
2023-11-21 21:12:18 +0700

    BTnhomWeek3

---
+++ 2.Tuan3-21.11.23/baiTap-nhom2.js
@@ -0,0 +1,23 @@
+// Bài 5
+// let arr = []
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = +prompt("Nhập số thứ " +i);
+// }
+// function checkMin(n){
+// let min =  n[0];
+// for (let i = 1; i < n.length; ++i) {
+//     if (min > n[i]) {
+//         min = n[i];
+//     }
+// }
+// document.write("Giá trị nhỏ nhất là ",min);
+// }
+// checkMin(arr)
+
+// Bài 6
+
+let num=+prompt("Nhập vào một số")
+function isInteger(n) {
+    return n>0;
+}
+document.write(isInteger(num))
